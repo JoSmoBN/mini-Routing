51bf442d768373e088ad657c26e808fe19fda330
Joseph Nolan <josephbnolan@me.com>
2016-02-29 14:44:27 -0600

    finished mini project

---
+++ js/products/productsCtrl.js
@@ -0,0 +1,11 @@
+angular.module('miniRouting')
+  .controller('productsCtrl', function($scope, productsService, $stateParams) {
+
+    if ($stateParams.id === 'shoes') {
+        $scope.productData = productsService.shoeData;
+    }
+    else if ($stateParams.id === 'socks') {
+        $scope.productData = productsService.sockData;
+    }
+
+});
